fef3259e2cfc0f760269ad99d5457057ec64a6e7
Mike Lloyd <mlloyd@pivotal.io>
2018-08-07 08:37:57 -0600

    added OAuth2Endpoint configs and updated interface with docs.
    Signed-off-by: Mike Lloyd <mlloyd@pivotal.io>

--- src/IOAuthClient.cs
+++ src/IOAuthClient.cs
@@ -1,22 +1,28 @@
-﻿using System.Collections.Generic;
-using System.Net.Http;
+﻿using System.Net.Http;
+using System.Threading.Tasks;
 
-namespace oauth
+namespace OAuth2
 {
-    public interface IOAuthClient : IHttpClientFactory
+    /// <summary>
+    /// Base interface for an OAuth2 Client for 2-legged authentication.
+    /// </summary>
+    public interface IOAuth2Client : IHttpClientFactory
     {
-        HttpClient CreateClient(string name, OauthEndpoint serverConfig);
-        void Refresh();
-
-    }
-
-    public struct OauthEndpoint
-    {
-        public string ClientId;
-        public string ClientSecret;
-        public string TokenUrl;
-        public string RefreshUrl;
-        public List<string> Scopes;
-        public Dictionary<string, string> EndpointParameters;
+        /// <summary>
+        /// Create a new client.
+        /// </summary>
+        /// <param name="name">The desired name of the client. This will also be used as the User-Agent header.</param>
+        /// <param name="serverConfig">OAuth2 configurations.</param>
+        /// <returns>HttpClient</returns>
+        HttpClient CreateClient(string name, OAuth2Endpoint oAuth2Endpoint);
+        /// <summary>
+        /// Request a new Refresh Token.
+        /// </summary>
+        Task Refresh();
+        /// <summary>
+        /// Get the current config of the instantiated HttpClient.
+        /// </summary>
+        /// <returns></returns>
+        OAuth2Endpoint GetConfig();
     }
 }
---
+++ src/OAuth2Endpoint.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace OAuth2
+{
+    /// <summary>
+    /// Target configurations for the client.
+    /// </summary>
+    public struct OAuth2Endpoint
+    {
+        /// <summary>
+        /// Your Client ID. Sometimes known as an AppID or Application ID.
+        /// </summary>
+        public string ClientId;
+        /// <summary>
+        /// Your Client Secret. Sometimes known as an App Password.
+        /// </summary>
+        public string ClientSecret;
+        /// <summary>
+        /// The full URL path where the HttpClient can request a Token for authentication.
+        /// </summary>
+        public string TokenUrl;
+        /// <summary>
+        /// The full URL path where the HttpClient can request a new Refresh Token.
+        /// </summary>
+        public string RefreshUrl;
+        /// <summary>
+        /// Any scopes required for authorization of any resources needed. Check with your OAuth2 provider for details on scopes required.
+        /// </summary>
+        public List<string> Scopes;
+        /// <summary>
+        /// Any extra parameters necessary that need to be shipped with requests.
+        /// </summary>
+        public Dictionary<string, string> EndpointParameters;
+    }
+}
