b9a57a5fd0173a90754d84094eacc7273771b533
Mike Lloyd <mlloyd@pivotal.io>
2018-08-07 08:52:49 -0600

    added extensions reference.
    Signed-off-by: Mike Lloyd <mlloyd@pivotal.io>

---
+++ src/OAuth2Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OAuth2
+{
+    class OAuth2Client : IOAuth2Client
+    {
+        public OAuth2Endpoint _oAuth2Endpoint;
+        public HttpClient CreateClient(string name, OAuth2Endpoint oAuth2Endpoint)
+        {
+            _oAuth2Endpoint = oAuth2Endpoint;
+            throw new NotImplementedException();
+        }
+
+        public HttpClient CreateClient(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OAuth2Endpoint GetConfig()
+        {
+            return _oAuth2Endpoint;
+        }
+
+        public Task Refresh()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
